fa172e9350bb38cb958a62fedee32a136deab434
Hristo <owner@iMac.local>
2020-10-09 13:07:51 -0700

    add display repos.

--- ui.js
+++ ui.js
@@ -32,6 +32,31 @@ class UI {
     `;
   }
 
+  //  show user repos
+  showRepos(repos) {
+    let output = '';
+
+    repos.forEach(function (repo) {
+      output += `
+      <div class="card card-body mb-2">
+        <div class="row">
+          <div class="col-md-6">
+            <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+          </div>
+          <div class="col-md-6">
+            <span class="badge badge-primary mb-2">Stars: ${repo.stargazers_count}</span>
+            <span class="badge badge-secondary mb-2">Watchers: ${repo.watchers}</span>
+            <span class="badge badge-success mb-2">Forks: ${repo.forms_count}</span>
+          </div>
+        </div>
+      </div>
+      `;
+    });
+
+    // Output Repositories
+    document.getElementById('repos').innerHTML = output;
+  }
+
   // Show alert message
   showAlert(message, className) {
     // Clear remaining alerts
